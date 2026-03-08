96f7de140360b8797dc45846a81e59d733573fd8
Sonya Sarver <smsarver@gmail.com>
2026-03-08 11:58:14 -0700

    Set collector email in site config

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 window.GALLERY_CONFIG = {
   askArtistEndpoint: "https://formspree.io/f/REPLACE_WITH_YOUR_FORM_ID",
   askArtistUseAjax: false,
-  collectorEmail: "you@example.com",
+  collectorEmail: "smsarver@gmail.com",
   supabaseUrl: "https://yvzbdxxsmxnmgtbohecz.supabase.co",
   supabaseAnonKey: "sb_publishable_YmKFGTo3-nUx6kh5zegElA__CZBeSij"
 };
